c918f8b57b52db605b7b19bf58bde19ef91c88ce
Leandro Gonçalves <leandrogoncalvespr@hotmail.com>
2024-02-02 19:47:04 -0300

    feat: asd

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs';
 import fetch from 'cross-fetch';
 globalThis.fetch = fetch;
 
 async function bootstrap() {
+  const httpsOptions = {
+    key: fs.readFileSync('./src/cert/key.pem'),
+    cert: fs.readFileSync('./src/cert/cert.pem'),
+  };
+
+  console.log(httpsOptions);
+
   const app = await NestFactory.create(AppModule, {
     cors: { origin: '*' },
+    httpsOptions,
   });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
